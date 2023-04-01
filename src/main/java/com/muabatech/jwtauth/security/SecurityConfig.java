f64eb5b540da36eca727fe482a1e85a0963aef5e
Muuyi Andrew <muuyiandrew2015@gmail.com>
2023-04-01 04:06:12 +0300

    Usedetails

--- src/main/java/com/muabatech/jwtauth/security/SecurityConfig.java
+++ src/main/java/com/muabatech/jwtauth/security/SecurityConfig.java
@@ -1,5 +1,34 @@
 package com.muabatech.jwtauth.security;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		http
+		.csrf().disable()
+		//.authorizeRequests()
+		.antMatchers(HttpMethod.GET).permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.httpBasic();
+		
+		return http.build();
+		
+	}
+	@Bean
+	public UserDetailsService users() {
+		UserDetails admin = User.builder().username("")
+	}
 }
