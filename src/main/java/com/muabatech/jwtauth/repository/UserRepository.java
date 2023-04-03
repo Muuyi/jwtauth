f0753a39d8ebfef24043f124dbc8a59beb7a5620
Muuyi Andrew <muuyiandrew2015@gmail.com>
2023-04-03 05:50:30 +0300

    Entities

---
+++ src/main/java/com/muabatech/jwtauth/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.muabatech.jwtauth.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.muabatech.jwtauth.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+	Optional<UserEntity> findByUsername(String username);
+	Boolean existsByUsername(String username);
+}
